b64736030d026c5d6d043471040fc83cb747cbc4
emilyvera <evera5@illinois.edu>
2018-09-11 12:19:28 -0500

    random ?

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -20,7 +20,7 @@ public class SolveMaze {
         /*
          * Create a new 10 x 10 maze. Feel free to change these values.
          */
-        Maze maze = new Maze(10, 10);
+        Maze maze = new Maze(100, 100);
 
         /*
          * Pick (0, 0), the bottom left corner, as the starting point.
@@ -34,7 +34,26 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            /**while (!maze.isFinished()) {
+                maze.turnLeft();
+                while (!maze.canMove()) {
+                    maze.turnRight();
+                }
+                maze.move();
+            }*/
+            while (!maze.isFinished()) {
+                if (maze.canMove()) {
+                    maze.move();
+                }
+                else {
+                    int pick = (int) (Math.random() * (1) + 1);
+                    if (pick == 0) {
+                        maze.turnLeft();
+                    } else {
+                        maze.turnRight();
+                    }
+                }
+            }
         }
 
         if (maze.isFinished()) {
